9b0da2ba5b46a58e9163c53ba2133c7fd0eaa916
codesinging <codesinging@gmail.com>
2022-02-22 00:27:01 +0800

    添加独立的 admin 路由文件

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('test', fn() => 'admin');
