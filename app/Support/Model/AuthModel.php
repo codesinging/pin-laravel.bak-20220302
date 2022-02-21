d7493e58c1667a3215f3d0f3e94d064768b88797
codesinging <codesinging@gmail.com>
2022-02-22 00:38:04 +0800

    添加基础模型类和基础认证模型类

---
+++ app/Support/Model/AuthModel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace App\Support\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class AuthModel extends User
+{
+    use HasFactory;
+    use HasApiTokens;
+    use Notifiable;
+}
---
+++ app/Support/Model/BaseModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace App\Support\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    use HasFactory;
+}
