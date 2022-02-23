c1614d5d02e1e5ca5f12de6cfcad62d8421e06c7
codesinging <codesinging@gmail.com>
2022-02-23 16:12:08 +0800

    增加 ListHelpers 测试

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,8 @@
  */
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin;
 
 Route::get('test', fn() => 'admin');
+
+Route::apiResource('admins', Admin\AdminsController::class);
---
+++ tests/Feature/Support/Model/ListHelpersTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Support\Model;
+
+use App\Models\Admin;
+use Database\Seeders\AdminSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListHelpersTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testListerWhenPageable()
+    {
+        $this->seed(AdminSeeder::class);
+
+        request()->merge([
+            'pageable' => true,
+            'size' => 1,
+        ]);
+
+        $lister = (new Admin())->lister();
+
+        self::assertIsArray($lister);
+        self::assertArrayHasKey('pageable', $lister);
+        self::assertArrayHasKey('data', $lister);
+        self::assertArrayHasKey('total', $lister);
+        self::assertArrayHasKey('page', $lister);
+        self::assertArrayHasKey('size', $lister);
+        self::assertArrayHasKey('from', $lister);
+        self::assertArrayHasKey('to', $lister);
+
+        self::assertTrue($lister['pageable']);
+        self::assertEquals(2, $lister['total']);
+        self::assertEquals(1, $lister['size']);
+        self::assertTrue($lister['more']);
+    }
+
+    public function testListerWhenNotPageable()
+    {
+        $this->seed(AdminSeeder::class);
+
+        request()->merge([
+            'pageable' => false,
+        ]);
+
+        $lister = (new Admin())->lister();
+
+        self::assertIsArray($lister);
+        self::assertArrayHasKey('pageable', $lister);
+        self::assertArrayHasKey('data', $lister);
+        self::assertArrayHasKey('total', $lister);
+
+        self::assertFalse($lister['pageable']);
+        self::assertEquals(2, $lister['total']);
+    }
+}
