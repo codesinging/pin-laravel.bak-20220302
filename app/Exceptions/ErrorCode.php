e6b987a80970be462c7ef38dd743317986f97f19
codesinging <codesinging@gmail.com>
2022-02-23 17:46:51 +0800

    添加错误代码字典类

---
+++ app/Exceptions/ErrorCode.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace App\Exceptions;
+
+class ErrorCode
+{
+    const OK = 0;
+
+    const VALIDATION_ERROR = 90100;
+}
