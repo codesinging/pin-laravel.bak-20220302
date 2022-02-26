5890cf71a73cf54a6790195d20ef333e91b501e4
codesinging <codesinging@gmail.com>
2022-02-26 12:34:42 +0800

    增加 RouteParser，获取路由相关信息

---
+++ app/Support/Routing/RouteParser.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace App\Support\Routing;
+
+use App\Support\Reflection\ClassReflection;
+use Illuminate\Routing\Route as RouteClass;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use ReflectionException;
+
+class RouteParser
+{
+    protected RouteClass $route;
+
+    protected string $module;
+    protected string $controller;
+    protected string $controllerTitle;
+    protected string $action;
+    protected string $actionTitle;
+
+    /**
+     * @param RouteClass|string $route
+     */
+    public function __construct(RouteClass|string $route)
+    {
+        $this->route = is_string($route) ? self::route($route) : $route;
+        $this->parse();
+    }
+
+    /**
+     * @param string|null $prefix
+     *
+     * @return RouteClass[]
+     */
+    public static function routes(string $prefix = null): array
+    {
+        /** @var RouteClass[] $routes */
+        $routes = Route::getRoutes();
+
+        return $prefix
+            ? collect($routes)->filter(fn(RouteClass $route) => $route->getPrefix() === $prefix)->toArray()
+            : $routes;
+    }
+
+    /**
+     * @param string $action
+     *
+     * @return RouteClass|null
+     */
+    public static function route(string $action): ?RouteClass
+    {
+        return Route::getRoutes()->getByAction($action);
+    }
+
+    /**
+     * @return void
+     * @throws ReflectionException
+     */
+    private function parse()
+    {
+        list($class, $action) = explode('@', $this->route->getActionName());
+
+        $this->action = $action;
+
+        $reflection = new ClassReflection($class);
+
+        $this->controllerTitle = $reflection->classTitle();
+        $this->actionTitle = $reflection->methodTitle($action);
+
+        $name = Str::after($class, 'App\\Http\\Controllers\\');
+        $name = Str::beforeLast($name, 'Controller');
+
+        if (str_contains($name, '\\')){
+            $this->controller = Str::afterLast($name, '\\');
+            $this->module = Str::beforeLast($name, '\\');
+        } else {
+            $this->controller = $name;
+            $this->module = '';
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function module(): string
+    {
+        return $this->module;
+    }
+
+    /**
+     * @return string
+     */
+    public function controller(): string
+    {
+        return $this->controller;
+    }
+
+    /**
+     * @return string
+     */
+    public function controllerTitle(): string
+    {
+        return $this->controllerTitle;
+    }
+
+    /**
+     * @return string
+     */
+    public function action(): string
+    {
+        return $this->action;
+    }
+
+    /**
+     * @return string
+     */
+    public function actionTitle(): string
+    {
+        return $this->actionTitle;
+    }
+}
---
+++ tests/Feature/Support/Routing/RouteParserTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace Tests\Feature\Support\Routing;
+
+use App\Http\Controllers\Admin\AuthController;
+use App\Support\Routing\RouteParser;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class RouteParserTest extends TestCase
+{
+    protected string $routeAction = AuthController::class . '@login';
+
+    protected string $routeClass = AuthController::class;
+
+    public function testRoutes()
+    {
+        $routes = RouteParser::routes('api/admin');
+
+        /** @var Route $route */
+        $route = Arr::random($routes);
+
+        self::assertEquals('api/admin', $route->getPrefix());
+    }
+
+    public function testRoute()
+    {
+        self::assertInstanceOf(Route::class, RouteParser::route($this->routeAction));
+    }
+
+    public function testRouteParser()
+    {
+        $parser = new RouteParser($this->routeAction);
+
+        self::assertEquals('Admin', $parser->module());
+        self::assertEquals('Auth', $parser->controller());
+        self::assertEquals('login', $parser->action());
+        self::assertEquals('用户认证', $parser->controllerTitle());
+        self::assertEquals('管理员登录', $parser->actionTitle());
+    }
+}
